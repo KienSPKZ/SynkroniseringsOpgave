288fe33f70d0a7f5693a548183556c55c98df799
jacobgh00 <Jacobgh00@gmail.com>
2023-09-25 09:30:15 +0200

    Server

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,2 +1,20 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class TCPClient {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket connectionToDNSSocket =  new Socket("localhost", 1234);
+        BufferedReader inFromDNSServer = new BufferedReader(new InputStreamReader(connectionToDNSSocket.getInputStream()));
+        DataOutputStream outTODNSServer = new DataOutputStream(connectionToDNSSocket.getOutputStream());
+
+
+    }
+
+
+
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,2 +1,24 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPServer {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket socket = new ServerSocket(1234);
+
+
+        Socket connection = socket.accept();
+        System.out.println("Connection from: " + connection);
+
+        BufferedReader inToCLient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connection.getOutputStream());
+
+
+
+    }
 }
